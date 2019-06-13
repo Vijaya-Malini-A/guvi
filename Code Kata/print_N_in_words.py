fc0fd397350d400420108afd93a17d0796790c1f
Vijaya-Malini-A <51663604+Vijaya-Malini-A@users.noreply.github.com>
2019-06-13 16:32:12 +0530

    Create print_N_in_words.py

---
+++ Code Kata/print_N_in_words.py
@@ -0,0 +1,21 @@
+n = int(raw_input())
+if n is 1:
+  print "One"
+elif n is 2:
+  print "Two"
+elif n is 3:
+  print "Three"
+elif n is 4:
+  print "Four"
+elif n is 5:
+  print "Five"
+elif n is 6:
+  print "Six"
+elif n is 7:
+  print "Seven"
+elif n is 8:
+  print "Eight"
+elif n is 9:
+  print "Nine"
+elif n is 10:
+  print "Ten"
