8dfeea7cf14e394f5733a6b65ac00ef782b26794
Vijaya Malini A <51663604+Vijaya-Malini-A@users.noreply.github.com>
2019-06-16 21:21:59 +0530

    Create check_3_points_lie_on_the_same_line.py

---
+++ Code Kata/check_3_points_lie_on_the_same_line.py
@@ -0,0 +1,7 @@
+x1, y1 = map(int,raw_input().split())
+x2, y2 = map(int,raw_input().split())
+x3, y3 = map(int,raw_input().split())
+if x1 == x2 == x3 or y1 == y2 == y3:
+  print "yes"
+else:
+  print "no"
