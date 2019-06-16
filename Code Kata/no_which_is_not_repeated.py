c970d995fd295bbfd5cc144c4040dbe4ac3c454b
Vijaya Malini A <51663604+Vijaya-Malini-A@users.noreply.github.com>
2019-06-16 19:22:16 +0530

    Create no_which_is_not_repeated.py

---
+++ Code Kata/no_which_is_not_repeated.py
@@ -0,0 +1,8 @@
+n = int(raw_input())
+li = map(int,raw_input().split())
+count = []
+for i in range(len(li)):
+  count.append(li.count(li[i]))
+for i in range(len(count)):
+  if count[i] is 1:
+    print li[i]
