b4f74b15dc9f7b0a35ebc433b3daf6f68f661094
Vijaya Malini A <51663604+Vijaya-Malini-A@users.noreply.github.com>
2019-06-16 20:47:06 +0530

    Update encode_the_str_by_adding_3_to_each_char.py

--- Code Kata/encode_the_str_by_adding_3_to_each_char.py
+++ Code Kata/encode_the_str_by_adding_3_to_each_char.py
@@ -1,5 +1,18 @@
 s = raw_input()
 s1 = ""
 for i in range(len(s)):
-  s1 += chr(ord(s[i]) + 3)
+  ascii = ord(s[i]) + 3
+  if ascii == 123:
+    ascii = 97
+  elif ascii == 124:
+    ascii = 98
+  elif ascii == 125:
+    ascii = 99
+  elif ascii == 91:
+    ascii = 65
+  elif ascii == 92:
+    ascii = 66
+  elif ascii == 93:
+    ascii = 67
+  s1 += chr(ascii)
 print s1
