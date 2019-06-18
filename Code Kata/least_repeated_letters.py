634b31088c8d2f15b7fb20a412a8c0ae613f27fc
Vijaya Malini A <51663604+Vijaya-Malini-A@users.noreply.github.com>
2019-06-18 07:44:42 +0530

    Update least_repeated_letters.py

--- Code Kata/least_repeated_letters.py
+++ Code Kata/least_repeated_letters.py
@@ -1,14 +1,12 @@
 s = input()
-s1, s2 = "", ""
+s1 = ""
 count = []
 for i in range(len(s)):
-  s1 += s[i].lower()
+  if s[i] not in s1 and s[i] != " ":
+    s1 += s[i]
 for i in range(len(s1)):
-  if s1[i] not in s2 and s1[i] != " ":
-    s2 += s1[i]
-for i in range(len(s2)):
-  count.append(s1.count(s2[i]))
+  count.append(s.count(s1[i]))
 mini = min(count)
 for i in range(len(count)):
   if mini == count[i]:
-    print(s2[i],end = " ")
+    print(s1[i],end = " ")
