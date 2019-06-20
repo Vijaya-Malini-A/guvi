485d29f9fc4f815b531787a7458260c713fc73ac
Vijaya Malini A <51663604+Vijaya-Malini-A@users.noreply.github.com>
2019-06-20 09:59:32 +0530

    Update int_or_long.py

--- Code Kata/int_or_long.py
+++ Code Kata/int_or_long.py
@@ -1,3 +1,7 @@
 n = int(input())
-if type(n) == int:
+if n >= -32767 and n <= 32767:
   print("INT")
+elif n >= -2147483647 and n <= 2147483647:
+  print("LONG")
+elif n >= -9223372036854775807 and n <= 9223372036854775807:
+  print("LONG LONG")
