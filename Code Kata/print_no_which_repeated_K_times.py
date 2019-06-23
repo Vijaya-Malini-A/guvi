4ea758c457f4c619f1d2fef481893b23ca1b2f7e
Vijaya Malini A <51663604+Vijaya-Malini-A@users.noreply.github.com>
2019-06-23 14:31:19 +0530

    Update print_no_which_repeated_K_times.py

--- Code Kata/print_no_which_repeated_K_times.py
+++ Code Kata/print_no_which_repeated_K_times.py
@@ -1,7 +1,7 @@
 n, k = list(map(int,input().split()))
 li = list(map(int,input().split()))
-s = ""
+s1 = ""
 for i in range(len(li)):
-  if li.count(li[i]) == k and str(li[i]) not in s:
-    s += str(li[i])
+  if li.count(li[i]) == k and str(li[i]) not in s1:
+    s1 += str(li[i])
     print(li[i],end = " ")
