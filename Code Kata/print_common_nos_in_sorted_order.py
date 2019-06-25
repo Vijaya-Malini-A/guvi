1024015903b1b2cb2112d8599bfa2c1874322ba1
Vijaya Malini A <51663604+Vijaya-Malini-A@users.noreply.github.com>
2019-06-25 10:28:21 +0530

    Create print_common_nos_in_sorted_order.py

---
+++ Code Kata/print_common_nos_in_sorted_order.py
@@ -0,0 +1,11 @@
+n, m = list(map(int,input().split()))
+li = list(map(int,input().split()))
+s1 = li[:n]
+s2 = li[n:]
+s = []
+for i in range(len(s1)):
+  if s1[i] in s2 and s1[i] not in s:
+    s.append(s1[i])
+s.sort()
+for i in range(len(s)):
+  print(s[i],end = " ")
