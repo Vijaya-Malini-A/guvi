0684965f7ad79b68a732eaa204e5d6da4d965de3
Vijaya Malini A <51663604+Vijaya-Malini-A@users.noreply.github.com>
2019-06-26 10:07:22 +0530

    Create no_of_occurance_of_each_char_and_print_in_decreasing_order.py

---
+++ Code Kata/no_of_occurance_of_each_char_and_print_in_decreasing_order.py
@@ -0,0 +1,16 @@
+n = int(input())
+li = list(map(int,input().split()))
+unique = []
+count = []
+li.sort()
+li = li[::-1]
+for i in range(n):
+  if li[i] not in unique:
+    unique.append(li[i])
+for i in range(len(unique)):
+  count.append(li.count(unique[i]))
+for i in range(len(count)):
+  ind = count.index(max(count))
+  print(unique[ind],end = " ")
+  unique.remove(unique[ind])
+  count.remove(count[ind])
