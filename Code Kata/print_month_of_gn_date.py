2875c4bb73a0d173abac5b55de59917d7ee74b1f
Vijaya Malini A <51663604+Vijaya-Malini-A@users.noreply.github.com>
2019-06-26 10:40:32 +0530

    Create print_month_of_gn_date.py

---
+++ Code Kata/print_month_of_gn_date.py
@@ -0,0 +1,26 @@
+s = input()
+mm = s[3:5]
+if mm == "01":
+  print("January")
+elif mm == "02":
+  print("February")
+elif mm == "03":
+  print("March")
+elif mm == "04":
+  print("April")
+elif mm == "05":
+  print("May")
+elif mm == "06":
+  print("June")
+elif mm == "07":
+  print("July")
+elif mm == "08":
+  print("August")
+elif mm == "09":
+  print("September")
+elif mm == "10":
+  print("October")
+elif mm == "11":
+  print("November")
+elif mm == "12":
+  print("December")
