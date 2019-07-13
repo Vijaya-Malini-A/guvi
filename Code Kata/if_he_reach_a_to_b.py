a12804184768c5321ed9877ce9e861e20cd6f4b3
Vijaya Malini A <51663604+Vijaya-Malini-A@users.noreply.github.com>
2019-07-13 20:18:24 +0530

    Create if_he_reach_a_to_b.py

---
+++ Code Kata/if_he_reach_a_to_b.py
@@ -0,0 +1,12 @@
+n, m = list(map(int,input().split()))
+x = 0
+for i in range(n):
+  a, b = list(map(int,input().split()))
+  if b-a > 0 and b-a <= m:
+    x = 1
+  else:
+    break
+if x == 1:
+  print("yes")
+else:
+  print("no") 
