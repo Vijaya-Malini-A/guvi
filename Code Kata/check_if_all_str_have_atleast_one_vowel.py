bddc84723a6f6c03c6da920720e8a5f81eb9ee50
Vijaya Malini A <51663604+Vijaya-Malini-A@users.noreply.github.com>
2019-07-14 12:47:45 +0530

    Create check_if_all_str_have_atleast_one_vowel.py

---
+++ Code Kata/check_if_all_str_have_atleast_one_vowel.py
@@ -0,0 +1,11 @@
+n = int(input())
+x = 0
+for i in range(n):
+  s = input()
+  if 'a' in s or 'e' in s or 'i' in s or 'o' in s or 'u' in s:
+    x += 1
+    continue
+if x == n:
+  print("yes")
+else:
+  print("no")
