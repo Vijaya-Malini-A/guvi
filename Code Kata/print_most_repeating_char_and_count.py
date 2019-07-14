68747c954323d4a04658a450c99839c46c6fed44
Vijaya Malini A <51663604+Vijaya-Malini-A@users.noreply.github.com>
2019-07-14 13:58:45 +0530

    Create print_most_repeating_char_and_count.py

---
+++ Code Kata/print_most_repeating_char_and_count.py
@@ -0,0 +1,13 @@
+s = input()
+s = list(s)
+s1, s2 = [], []
+s3 = ""
+for i in range(len(s)):
+  if s[i] not in s1:
+    s1.append(s[i])
+for i in range(len(s1)):
+  s2.append(s.count(s1[i]))
+for i in range(len(s2)):
+  if max(s2) == s2[i]:
+    s3 += s1[i]
+print(max(s2),s3)
