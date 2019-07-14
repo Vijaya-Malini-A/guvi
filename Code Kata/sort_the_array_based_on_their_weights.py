0316086b971e965700be32cfaf6f3cc7fdccb514
Vijaya Malini A <51663604+Vijaya-Malini-A@users.noreply.github.com>
2019-07-14 08:47:03 +0530

    Create sort_the_array_based_on_their_weights.py

---
+++ Code Kata/sort_the_array_based_on_their_weights.py
@@ -0,0 +1,9 @@
+n = int(input())
+li = list(map(int,input().split()))
+weight = list(map(int,input().split()))
+weight1 = weight[:]
+weight.sort()
+for i in range(n):
+  for j in range(n):
+    if weight[i] == weight1[j]:
+      print(li[j],end = " ")
